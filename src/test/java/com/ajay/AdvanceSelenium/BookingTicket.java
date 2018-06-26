b6c088479373de9f7557f8237e8d54485e7c3490
A06438_P5.Training <A06438_P5.Training@P5-GKQ1N62.dir.svc.accenture.com>
2018-06-26 17:07:42 +0530

    commit 3

--- src/test/java/com/ajay/AdvanceSelenium/BookingTicket.java
+++ src/test/java/com/ajay/AdvanceSelenium/BookingTicket.java
@@ -11,62 +11,120 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 
 public class BookingTicket {
 	private WebDriver driver;
+	private int colnum = 2;
+	private static int rownum = 1;
 
 	@DataProvider(name = "loginData")
 	public String[][] login_data() throws IOException {
 		ExcelUtility.setExcelPath("Sheet1", "C:\\Users\\A06438_p5.training\\Desktop\\testdata\\Book1.xlsx");
-		String userName = ExcelUtility.getCellData(1, 1);
-		String password = ExcelUtility.getCellData(1, 2);
-		return new String[][] { new String[] { userName, password } };
+
+		String[][] excelData = ExcelUtility.getExcelTable();
+		return excelData;
 	}
 
 	@Test(priority = 1, dataProvider = "loginData")
-	public void login(String userName, String password) {
-		LoginPageObject.uname.sendKeys(userName);
-		LoginPageObject.password.sendKeys(password);
-		LoginPageObject.login_button.click();
-		// assertEquals("Find a Flight: Mercury Tours:",driver.getTitle());
+	public void login(String userName, String password) throws IOException {
+		try {
+			LoginPageObject.uname.sendKeys(userName);
+			LoginPageObject.password.sendKeys(password);
+			LoginPageObject.login_button.click();
+			WebDriverWait wait = new WebDriverWait(driver, 50);
+			wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.name("tripType")));
+			assertEquals("Find a Flight: Mercury Tours:", driver.getTitle());
+			ExcelUtility.setExcelData(rownum, colnum, "pass");
+			rownum++;
+			driver.get("http://newtours.demoaut.com/");
+			driver.manage().window().maximize();
+		} catch (AssertionError e) {
+			ExcelUtility.setExcelData(rownum, colnum, "fail");
+		}
 
 	}
 
-	@Test(priority = 2)
-	public void bookTicket() {
-		driver.findElement(By.cssSelector(
-				"body > div:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(2) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(4) > td:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(2) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(5) > td:nth-child(1) > form:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(2) > td:nth-child(2) > b:nth-child(1) > font:nth-child(1) > input:nth-child(2)"))
-				.click();
-		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-		Select s = new Select(driver.findElement(By.cssSelector(
-				"html body div table tbody tr td table tbody tr td table tbody tr td table tbody tr td form table tbody tr td b select")));
-		s.selectByValue("3");
-		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-		Select s1 = new Select(driver.findElement(By.cssSelector(
-				"body > div:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(2) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(4) > td:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(2) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(5) > td:nth-child(1) > form:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(4) > td:nth-child(2) > select:nth-child(1)")));
-		s1.selectByValue("London");
-		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-		Select s2 = new Select(driver.findElement(By.cssSelector(
-				"body > div:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(2) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(4) > td:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(2) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(5) > td:nth-child(1) > form:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(5) > td:nth-child(2) > select:nth-child(1)")));
-		s2.selectByValue("2");
-		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-		Select s3 = new Select(driver.findElement(By.cssSelector(
-				"body > div:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(2) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(4) > td:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(2) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(5) > td:nth-child(1) > form:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(5) > td:nth-child(2) > select:nth-child(2)")));
-		s3.selectByValue("1");
-		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-		driver.findElement(By.cssSelector(
-				"body > div:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(2) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(4) > td:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(2) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(5) > td:nth-child(1) > form:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(9) > td:nth-child(2) > font:nth-child(1) > font:nth-child(2) > input:nth-child(4)"));
-		Select s4 = new Select(driver.findElement(By.cssSelector(
-				"body > div:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(2) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(4) > td:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(2) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(5) > td:nth-child(1) > form:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(10) > td:nth-child(2) > select:nth-child(1)")));
-		s4.selectByVisibleText("Blue Skies Airlines");
-		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-		driver.findElement(By.name("findFlights")).click();
-
-	}
+	// @Test(priority = 2)
+	// public void bookTicket() {
+	// // driver.findElement(By.cssSelector(
+	// // "body > div:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) >
+	// // tr:nth-child(1) > td:nth-child(2) > table:nth-child(1) >
+	// tbody:nth-child(1) >
+	// // tr:nth-child(4) > td:nth-child(1) > table:nth-child(1) >
+	// tbody:nth-child(1) >
+	// // tr:nth-child(1) > td:nth-child(2) > table:nth-child(1) >
+	// tbody:nth-child(1) >
+	// // tr:nth-child(5) > td:nth-child(1) > form:nth-child(1) > table:nth-child(1)
+	// >
+	// // tbody:nth-child(1) > tr:nth-child(2) > td:nth-child(2) > b:nth-child(1) >
+	// // font:nth-child(1) > input:nth-child(2)"))
+	// // .click();
+	// driver.findElement(By.cssSelector("input[value='oneway']")).click();
+	// driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+	// Select s = new Select(driver.findElement(By.cssSelector(
+	// "html body div table tbody tr td table tbody tr td table tbody tr td table
+	// tbody tr td form table tbody tr td b select")));
+	// s.selectByValue("3");
+	// driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+	// Select s1 = new Select(driver.findElement(By.cssSelector(
+	// "body > div:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) >
+	// tr:nth-child(1) > td:nth-child(2) > table:nth-child(1) > tbody:nth-child(1) >
+	// tr:nth-child(4) > td:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) >
+	// tr:nth-child(1) > td:nth-child(2) > table:nth-child(1) > tbody:nth-child(1) >
+	// tr:nth-child(5) > td:nth-child(1) > form:nth-child(1) > table:nth-child(1) >
+	// tbody:nth-child(1) > tr:nth-child(4) > td:nth-child(2) >
+	// select:nth-child(1)")));
+	// s1.selectByValue("London");
+	// driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+	// Select s2 = new Select(driver.findElement(By.cssSelector(
+	// "body > div:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) >
+	// tr:nth-child(1) > td:nth-child(2) > table:nth-child(1) > tbody:nth-child(1) >
+	// tr:nth-child(4) > td:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) >
+	// tr:nth-child(1) > td:nth-child(2) > table:nth-child(1) > tbody:nth-child(1) >
+	// tr:nth-child(5) > td:nth-child(1) > form:nth-child(1) > table:nth-child(1) >
+	// tbody:nth-child(1) > tr:nth-child(5) > td:nth-child(2) >
+	// select:nth-child(1)")));
+	// s2.selectByValue("2");
+	// driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+	// Select s3 = new Select(driver.findElement(By.cssSelector(
+	// "body > div:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) >
+	// tr:nth-child(1) > td:nth-child(2) > table:nth-child(1) > tbody:nth-child(1) >
+	// tr:nth-child(4) > td:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) >
+	// tr:nth-child(1) > td:nth-child(2) > table:nth-child(1) > tbody:nth-child(1) >
+	// tr:nth-child(5) > td:nth-child(1) > form:nth-child(1) > table:nth-child(1) >
+	// tbody:nth-child(1) > tr:nth-child(5) > td:nth-child(2) >
+	// select:nth-child(2)")));
+	// s3.selectByValue("1");
+	// driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+	// driver.findElement(By.cssSelector(
+	// "body > div:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) >
+	// tr:nth-child(1) > td:nth-child(2) > table:nth-child(1) > tbody:nth-child(1) >
+	// tr:nth-child(4) > td:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) >
+	// tr:nth-child(1) > td:nth-child(2) > table:nth-child(1) > tbody:nth-child(1) >
+	// tr:nth-child(5) > td:nth-child(1) > form:nth-child(1) > table:nth-child(1) >
+	// tbody:nth-child(1) > tr:nth-child(9) > td:nth-child(2) > font:nth-child(1) >
+	// font:nth-child(2) > input:nth-child(4)"));
+	// Select s4 = new Select(driver.findElement(By.cssSelector(
+	// "body > div:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) >
+	// tr:nth-child(1) > td:nth-child(2) > table:nth-child(1) > tbody:nth-child(1) >
+	// tr:nth-child(4) > td:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) >
+	// tr:nth-child(1) > td:nth-child(2) > table:nth-child(1) > tbody:nth-child(1) >
+	// tr:nth-child(5) > td:nth-child(1) > form:nth-child(1) > table:nth-child(1) >
+	// tbody:nth-child(1) > tr:nth-child(10) > td:nth-child(2) >
+	// select:nth-child(1)")));
+	// s4.selectByVisibleText("Blue Skies Airlines");
+	// driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+	// driver.findElement(By.name("findFlights")).click();
+	//
+	// }
 
 	@BeforeTest
 	public void beforeTest() {
--- src/test/java/com/ajay/AdvanceSelenium/ExcelUtility.java
+++ src/test/java/com/ajay/AdvanceSelenium/ExcelUtility.java
@@ -1,8 +1,11 @@
 package com.ajay.AdvanceSelenium;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
+import org.apache.poi.ss.usermodel.Row.MissingCellPolicy;
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
@@ -29,4 +32,36 @@ public class ExcelUtility {
 		return stringCellData;
 
 	}
+
+	public static String[][] getExcelTable() {
+		int noOfRows = sheet.getPhysicalNumberOfRows();
+		int numOfCols = 2;
+		int ci, cj;
+		ci = 1;
+		cj = 1;
+		String[][] excelData = new String[noOfRows][numOfCols];
+		for (int i = 0; i < noOfRows; i++, ci++) {
+			cj = 1;
+			for (int j = 0; j < numOfCols; j++, cj++) {
+				excelData[i][j] = getCellData(ci, cj);
+			}
+		}
+		return excelData;
+	}
+
+	public static void setExcelData(int rownum, int column, String data) throws IOException {
+		row = sheet.getRow(rownum);
+		cell = row.getCell(column, MissingCellPolicy.RETURN_BLANK_AS_NULL);
+		if (cell == null) {
+			cell = row.createCell(column);
+			cell.setCellValue(data);
+		} else {
+			cell.setCellValue(data);
+		}
+		FileOutputStream fos = new FileOutputStream("C:\\Users\\A06438_p5.training\\Desktop\\testdata\\Book1.xlsx");
+		wBook.write(fos);
+		fos.flush();
+		fos.close();
+	}
+
 }
